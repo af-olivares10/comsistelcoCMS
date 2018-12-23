8a0575f8c87be42f8038f4c90e3950941f428c0e
Andrés <af.olivares10@uniandes.edu.co>
2018-12-22 23:46:50 -0500

    Add editable picture to page

--- src/Selector.jsx
+++ src/Selector.jsx
@@ -12,7 +12,8 @@ class Selector extends Component {
       error:"",
       deleteModalVisible:false,
       editModalVisible:false,
-      name:null
+      name:null,
+      image:""
     };
   }
   componentDidMount(){
@@ -23,7 +24,7 @@ class Selector extends Component {
 
 }
 handleSelection = (value)=> {
-  this.setState({name:JSON.parse(value).name});
+  this.setState({name:JSON.parse(value).name, image:JSON.parse(value).image});
   this.props.pageSelected(value);
 
 }
@@ -40,34 +41,37 @@ toggleNewPage=()=>{
   })
 }
 submitNewPage = (e)=>{
-
   e.preventDefault();
-  if(this.refs.newPageName.value.trim()!==""){
-    let newPage = {name:this.refs.newPageName.value, url:this.refs.newPageName.value.replace(/\s/g,''),sections:[]};
+
+  if(this.refs.newPageName.value.trim()!==""&& this.refs.image.files[0]){
     this.setState({loading:true,error:""});
-    fetch("https://intellgentcms.herokuapp.com/api/page",{
-      method: 'POST',
-      headers: {
-        'x-access-token':localStorage.getItem("ComToken"),
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body:JSON.stringify(newPage)
-    }).then(response=> response.json()).then(json=>{
-      if(json.success){
-        this.setState((prevState)=>{
-          let newPages = prevState.pages.push(newPage);
-          return {loading:false, newPages,error:""};
-        });
-      }
-      else{
-        this.setState({error:json.message, loading:false});
-      }
-    }).catch(err=>console.error(err));
+    this.uploadFile(this.refs.image.files[0]).then(json2 =>{
+      let newPage = {image:json2.imageUrl,name:this.refs.newPageName.value, url:this.refs.newPageName.value.replace(/\s/g,''),sections:[]};
+      fetch("https://intellgentcms.herokuapp.com/api/page",{
+        method: 'POST',
+        headers: {
+          'x-access-token':localStorage.getItem("ComToken"),
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(newPage)
+      }).then(response=> response.json()).then(json=>{
+        if(json.success){
+          this.setState((prevState)=>{
+            let newPages = prevState.pages.push(newPage);
+            return {loading:false, newPages,error:""};
+          });
+        }
+        else{
+          this.setState({error:json.message, loading:false});
+        }
+      }).catch(err=>console.error(err));
+    })
+
   }
 
   else{
-    this.setState({loading:false,error:"Ingrese un nombre"});
+    this.setState({loading:false,error:"Ingrese un nombre y suba una imagen"});
   }
 }
 showDeleteModal= ()=>{
@@ -76,14 +80,18 @@ showDeleteModal= ()=>{
 showEditModal= ()=>{
   this.setState({editModalVisible:true})
 }
-handleOkEdit = () => {
+ handleOkEdit = async  () => {
 
   if(this.refs.newName.value.trim() !==""){
+
+    let imageFile = this.refs.newImage.files[0];
+    let imageUrl = imageFile? (await this.uploadFile(imageFile)).imageUrl: "same";
+
     this.setState({
       loading: true,
       error:""
     });
-    let newPage = {name:this.refs.newName.value, url:this.refs.newName.value.replace(/\s/g,''),originalUrl:this.state.name.replace(/\s/g,'')};
+    let newPage = {name:this.refs.newName.value, url:this.refs.newName.value.replace(/\s/g,''),originalUrl:this.state.name.replace(/\s/g,''), image:imageUrl};
 
     fetch("https://intellgentcms.herokuapp.com/api/page",{
       method: 'PUT',
@@ -159,6 +167,17 @@ handleOkDelete = () => {
       deleteModalVisible: false,
     });
   }
+  uploadFile = (file)=>{
+    let formData = new FormData();
+    formData.append("image",file);
+    return fetch('https://intellgentcms.herokuapp.com/api/uploadPicture', {
+      method: 'POST',
+      headers: {
+        'x-access-token':localStorage.getItem("ComToken")
+      },
+      body:formData
+    }).then( response => response.json())
+  }
   render() {
     return (
       <div className= "page-info">
@@ -177,6 +196,13 @@ handleOkDelete = () => {
           >
             <div className = "editModal">Nombre:</div>
             <input  type="text" ref = "newName" placeholder = "Nombre de la categoría" className="form-control" style ={{width:"80% !important",margin:"20px 0 20px 0"}} defaultValue ={this.state.name}></input>
+            <img alt="Imagen del producto" src={this.state.image} style={{ width: "100%", height: "40vh",marginLeft:"auto",marginRight:"auto",display:"block"}} />
+            <div className = " image-input-wrapper">
+              <div  className = "image-label">
+                 Cambiar imagen
+              </div>
+              <input id = "file-upload" className = "text-input image-input" type="file" ref = "newImage" name="myimages"  accept="image/*"  /><br/>
+            </div>
             <div style = {{"color":"red"}}>{this.state.error}</div>
           </Modal>
 
@@ -205,6 +231,12 @@ handleOkDelete = () => {
                   <span> Nueva página</span>
                 </div>
                 <input  type="text" ref = "newPageName" placeholder = "Nombre de la página" className="form-control text-input new-page-item " rows = "1"></input>
+                <div className = " image-input-wrapper">
+                  <div  className = "image-label">
+                     Imagen
+                  </div>
+                  <input id = "file-upload" className = "text-input images-input" type="file" ref = "image" name="myimages"  accept="image/*"  /><br/>
+                </div>
                 {this.state.loading?(
                   <div className="spinner">
                     <div className="double-bounce1"></div>
